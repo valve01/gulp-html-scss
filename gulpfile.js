038d689f91027490b4edb6793edf06b772b8188a
valve01 <118441831+valve01@users.noreply.github.com>
2023-09-29 10:40:27 +0300

    l

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,13 @@
-// const gulp = require('gulp');
 
-// gulp.task("hello", function(){
-// 	console.log("hello gulp")
-// })
 
-function defaultTask(cb) {
-	console.log("hello")
-	cb();
-}
-exports.default = defaultTask;
+const gulp = require('gulp');
+
+gulp.task('hello', function () {
+	console.log('hello gulp');
+});
+
+gulp.task('fuck', function () {
+	console.log('fuck you gulp');
+});
+
+
