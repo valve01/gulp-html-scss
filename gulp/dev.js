00f9ff678c85e750b7c87a7dafb1529ae01f3fda
valve01 <118441831+valve01@users.noreply.github.com>
2023-10-10 00:43:05 +0300

    1

--- gulp/dev.js
+++ gulp/dev.js
@@ -93,7 +93,7 @@ gulp.task('js:dev', function () {
 		.src('./js/*.js')
 		.pipe(changed('./build/js'))
 		.pipe(plumber(plumberConfig('JS')))
-		// .pipe(babel())
+		// .pipe(babel())//выключен в dev режиме
 		.pipe(webpack(require('./../webpack.config.js')))
 		.pipe(gulp.dest('./build/js'));
 });
