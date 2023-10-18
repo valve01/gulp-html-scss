02a2e3d22c63efbee0aa6f93f43c5b9fca032de5
valve01 <118441831+valve01@users.noreply.github.com>
2023-10-18 10:42:51 +0300

    1 COMMIT

--- src/js/index.js
+++ src/js/index.js
@@ -1,11 +1,2 @@
-import hello from './modules/hello';
-console.log('hello index');
-console.log(hello);
-
-import AirDatepicker from 'air-datepicker';
-import 'air-datepicker/air-datepicker.css';
-
-document.addEventListener('DOMContentLoaded', () => {
-	// new AirDatepicker('#my-element'[,options]);// есть еще настройки локали
-	new AirDatepicker('#date', { visible: false });
-});
+import mobileNav from './modules/mobile-nav.js';
+mobileNav();
